702b58e466f3daaacb8cacc39525546adb968773
Utku Cengiz <cekicengiz01@gmail.com>
2022-10-11 12:38:12 +0300

    javascript codes are completed in mobile view.

--- space-tourism-website-figma/assets/js/main.js
+++ space-tourism-website-figma/assets/js/main.js
@@ -1,3 +1,17 @@
 // mobil menu
 
-let mobilMenu = document.getElementById("mobil-menu")
+let mobilMenu = document.getElementById("mobil-menu");
+let mobilMenuContent = document.getElementById("mobil-menu-content");
+let closeButton = document.getElementById("close-button");
+
+mobilMenu.addEventListener("click", () => {
+  event.preventDefault();
+  mobilMenuContent.style.display = "block"
+
+});
+
+closeButton.addEventListener("click",() =>{
+    event.preventDefault();
+    mobilMenuContent.style.display = "none"
+
+})
