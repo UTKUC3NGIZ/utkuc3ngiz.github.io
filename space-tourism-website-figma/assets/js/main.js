9256d65c6f57cc0c48e7210fbecc79219d52600e
Utku Cengiz <cekicengiz01@gmail.com>
2022-11-04 02:17:37 +0300

    home page completed

--- space-tourism-website-figma/assets/js/main.js
+++ space-tourism-website-figma/assets/js/main.js
@@ -1,5 +1,5 @@
-const closed = document.getElementsByClassName("navbar__mobilMenu__inner--closed")[0]
-const mobilMenu = document.getElementsByClassName("navbar__mobilMenu__inner")[0]
+const closed = document.getElementsByClassName("navbar__inner--closed")[0]
+const mobilMenu = document.getElementsByClassName("navbar__inner")[0]
 const openMenu = document.getElementsByClassName("navbar__mobilMenu")[0]
 closed.addEventListener("click",() =>{
     mobilMenu.style.display = "none"
